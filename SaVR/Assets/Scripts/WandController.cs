c5c45556e75fb9a58e6aa664feb3c8287b863754
Andrew Luo <andrewjluo@gmail.com>
2016-04-25 13:53:29 -0700

    First commit

---
+++ SaVR/Assets/Scripts/WandController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WandController : MonoBehaviour {
+
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    public bool gripButtonDown = false;
+    public bool gripButtonUp = false;
+    public bool gripButtonPressed = false;
+
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    public bool triggerButtonDown = false;
+    public bool triggerButtonUp = false;
+    public bool triggerButtonPressed = false;
+
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    private SteamVR_TrackedObject trackedObj;
+
+	// Use this for initialization
+	void Start () {
+	    trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (controller == null)
+        {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+
+        gripButtonDown = controller.GetPressDown(gripButton);
+        gripButtonUp = controller.GetPressUp(gripButton);
+        gripButtonPressed = controller.GetPress(gripButton);
+
+        triggerButtonDown = controller.GetPressDown(triggerButton);
+        triggerButtonUp = controller.GetPressUp(triggerButton);
+        triggerButtonPressed = controller.GetPress(triggerButton);
+
+        if (gripButtonDown)
+        {
+            Debug.Log("Grip button was just pressed");
+        }
+
+        if (gripButtonUp)
+        {
+            Debug.Log("Grip Button was just unpressed");
+        }
+        if (triggerButtonDown)
+        {
+            Debug.Log("Trigger Button was just pressed");
+        }
+        if (triggerButtonUp)
+        {
+            Debug.Log("Trigger button was just unpressed");
+        }
+    }
+}
