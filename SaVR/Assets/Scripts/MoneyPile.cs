d9208853e853d972238eb2b8de06f9709863c1b6
Henry Tran <hhtran@cs.stanford.edu>
2016-04-26 15:48:11 -0700

    Added camera for local non-vr testing. Add gold pile prefab. Press X to generate bars

---
+++ SaVR/Assets/Scripts/MoneyPile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoneyPile : MonoBehaviour {
+	public GameObject goldBarPrefab;
+	public int batchSize = 100;
+	public int batchDelay = 10; //In milliseconds
+	public int barsToCreate = 100;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void createBars(int barsToCreate){
+		int barsCreated = 0;
+		Debug.Log ("Creating bars");
+
+		while (barsCreated < barsToCreate){
+			Vector3 spawnLocation = new Vector3 (transform.position.x, transform.position.y + barsCreated, transform.position.z);
+			Instantiate(goldBarPrefab, spawnLocation, Quaternion.identity);
+			barsCreated++;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.X)){
+			createBars(barsToCreate);
+		}
+	}
+}
